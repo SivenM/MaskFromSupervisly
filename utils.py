e900e7fa71684b4e3eca75a53ccb65fb629dc0f9
SivenM <51032516+SivenM@users.noreply.github.com>
2020-07-09 11:52:02 +0300

    исправление ошибок

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def get_img(img_path):
 
 
 def create_json(img, mask, image_name):
-    outdata = {'image': img, 'mask': mask}
+    outdata = {'image': img.tolist(), 'mask': mask.tolist()}
     with open(f'{image_name}.json', 'w') as outfile:
         json.dump(outdata, outfile)
 
